af81f3f1dbf81ac4fd68a219e817aff3ea8d3446
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2014-08-17 15:22:02 +0200

    feat: using now-and-later

--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,9 @@ var gulp = require('./gulp');
 
 function async(delay, message) {
   return function(done) {
+    console.log("Some async function started and going to say: ", message);
     setTimeout(function () {
-      console.log("Some async function finished saying: ", message);
+      console.log("Some async function finished after saying: ", delay, message);
       done(null, message);
     }, delay);
   };
@@ -13,13 +14,13 @@ function private(done) {
   return gulp.src('.gitignore').pipe(gulp.dest('dest'));
 }
 
-gulp.task('foo', async(400, 'foo'));
-gulp.task('bar', async(500, 'bar'));
-gulp.task('baz', async(500, 'baz'));
+gulp.task('foo', async(1000, 'foo'));
+gulp.task('bar', async(1000, 'bar'));
+gulp.task('baz', async(2000, 'baz'));
 
 gulp.task(function named() {
   console.log("Look, I'm a named function that acts as a task :-)");
   return 'from named';
 });
 
-gulp.task('default', gulp.series(gulp.parallel('foo', 'bar', private), 'baz', 'named'));
+gulp.task('default', gulp.series(gulp.parallel('foo', 'bar'), 'baz'));
